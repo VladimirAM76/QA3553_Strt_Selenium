c19595fdd1e761f7281fa41c5cd103cd159a6d39
VladimirAM76 <vladimir.am76@gmail.com>
2026-08-03 15:05:14 +0300

    login schema

---
+++ login_schema/test_login.py
@@ -0,0 +1,49 @@
+class Testlogin:
+
+    def setup_class(clc):
+
+        # open browser
+
+        # open site
+    def test_login_succsess(self):
+        # open form
+        # fill email
+        # fill password
+        # submit form
+
+
+    def test_login_succsess(self):
+        # open form
+        # fill email
+        # fill password
+        # submit form
+
+
+    def test_login_wrong_email(self):
+        # open form
+        # fill email
+        # fill password
+        # submit form
+
+
+    def test_login_wrong_passwords(self):
+        # open form
+        # fill email
+        # fill password
+        # submit form
+
+
+    def test_login_unregistered_user(self):
+        # open form
+        # fill email
+        # fill password
+        # submit form
+
+
+
+
+
+    def teardown_class(cls):
+        # close browser
+
+
