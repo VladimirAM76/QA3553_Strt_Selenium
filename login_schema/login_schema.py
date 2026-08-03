921220370fbb390eab45f77b3ca4ac3536dde183
VladimirAM76 <vladimir.am76@gmail.com>
2026-08-03 16:59:58 +0300

    login schema

--- login_schema/login_schema.py
+++ login_schema/login_schema.py
@@ -7,37 +7,37 @@ class Testlogin:
         # open site
     def test_login_succsess(self):
         # open form (find element + click)
-        # fill email
-        # fill password
-        # submit form
+        # fill email (find element + click + clear + type)
+        # fill password (find element + click + clear + type)
+        # submit form (find element + click)
 
 
     def test_login_succsess(self):
         # open form (find element + click)
-        # fill email
-        # fill password
-        # submit form
+        # fill email (find element + click + clear + type)
+        # fill password (find element + click + clear + type)
+        # submit form (find element + click)
 
 
     def test_login_wrong_email(self):
         # open form (find element + click)
-        # fill email
-        # fill password
-        # submit form
+        # fill email (find element + click + clear + type)
+        # fill password (find element + click + clear + type)
+        # submit form (find element + click)
 
 
     def test_login_wrong_passwords(self):
         # open form (find element + click)
-        # fill email
-        # fill password
-        # submit form
+        # fill email (find element + click + clear + type)
+        # fill password (find element + click + clear + type)
+        # submit form (find element + click)
 
 
     def test_login_unregistered_user(self):
         # open form (find element + click)
-        # fill email
-        # fill password
-        # submit form
+        # fill email (find element + click + clear + type)
+        # fill password (find element + click + clear + type)
+        # submit form (find element + click)
 
 
 
